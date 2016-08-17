32b1ce7f9e5c50ee16ed0839b82e5c9931de3a34
airbreak <jiangjianming_m@163.com>
2016-08-18 00:53:40 +0800

    add angular nav fn in page

--- Application/Home/Javascript/customer/myangular/index.js
+++ Application/Home/Javascript/customer/myangular/index.js
@@ -4,14 +4,45 @@
 
 var myMoudle=angular.module('MyFm',[]);
 
-myMoudle.controller('leftPanelController',['$scope',function($scope){
-    var ss='';
+myMoudle.controller('leftPanelController',['$scope','$http',function($scope,$http){
+
+    //音乐类型
     var promis=$http({
         method:'get',
         url:window.fmUrlObject.baseUrlApi+'getMusicTypeInfo'
-    }).success(function(data,status,headers,config){
-        $scope.musicTypes=data;
+    }).success(function(result,status,headers,config){
+        if(result.status=='success') {
+            $scope.musicTypes = result.data;
+        }
     }).error(function(data,status,headers,config){
         data;
     });
+
+
+    //banner
+    var promise=$http({
+        method:'get',
+        url:window.fmUrlObject.baseUrlApi+'getBannerInfo'
+    }).success(function(result,status,headers,config){
+        if(result.status=='success') {
+            var len = result.data.length,navArr=[];
+            for(var i=0;i<len;i++){
+                navArr.push(i+1);
+            }
+            //导航控制
+            $scope.navs=navArr;
+            $scope.isSelect=0;
+            $scope.selectMe=function($index){
+                $scope.isSelect=$index;
+                $scope.imgUrl=result.data[$index].imgName;
+            }
+            $scope.imgUrl=result.data[0].imgName;
+        }
+    }).error(function(data,status,headers,config){
+        data;
+    });
+
+
+
+
 }]);
